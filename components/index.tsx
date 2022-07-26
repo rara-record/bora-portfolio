fc0d44c378dcf7c4001eb8cfdde32cefa3d24829
bora <a52447879@gmail.com>
2022-07-26 21:15:25 +0900

    [feat] Hero, Animation 컴포넌트 추가

--- components/index.tsx
+++ components/index.tsx
@@ -1,3 +1,7 @@
 export { default as Layout } from "./Layout";
 export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
+
+// Home
+export { default as Hero } from "./home/Hero";
+export { default as Animation } from "./home/Animation";
