0a5ffe0e10e035127bdac9cda384ca96c624f099
bora <a52447879@gmail.com>
2022-07-26 21:31:21 +0900

    [feat] 다크모드 기능 및 DrkModeToggleButton 컴포넌트 추가

--- components/index.tsx
+++ components/index.tsx
@@ -1,6 +1,7 @@
 export { default as Layout } from "./Layout";
 export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
+export { default as DarkModeToggleButton } from "./UI/Button/DarkModeToggleButton";
 
 // Home
 export { default as Hero } from "./home/Hero";
