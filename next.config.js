60983c602807498dd1181bf12307af8f6141fcfd
bora <a52447879@gmail.com>
2022-08-02 04:15:05 +0900

    [chore] next.config 수정

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    domains: [
+      "www.notion.so",
+      "images.unsplash.com",
+      "s3.us-west-2.amazonaws.com",
+    ],
+    format: ["image/png", "image/webp", "image/jpeg"],
+  },
 };
 
 module.exports = nextConfig;
