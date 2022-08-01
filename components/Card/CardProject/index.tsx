de3cbbdebdacb4f44b4e53fce230a02a953b79af
bora <a52447879@gmail.com>
2022-08-02 06:18:02 +0900

    [design] 스타일 추가

--- components/Card/CardProject/index.tsx
+++ components/Card/CardProject/index.tsx
@@ -53,40 +53,75 @@ const CardProject = ({ data }: any) => {
               objectFit="cover"
               quality={100}
             />
-
-            {!loading && (
-              <div className="p-4 flex flex-col">
-                <h1 className="text-2xl font-bold">{title}</h1>
-                <h3 className="my-3 text-lg">{description}</h3>
-
-                <a href={github} target="_black" className=" py-2">
-                  깃허브 바로가기
-                </a>
-                <a href={notion} target="_black" className=" py-2">
-                  노션 보러가기
-                </a>
-
-                <p className="my-1 py-2">
-                  작업기간 : {start} ~ {end} ({calculatedPeriod(start, end)}일)
-                </p>
-                <div className="flex flex-wrap items-start gap-2 mt-2">
-                  {tags?.map((tag: any, index: number) => {
-                    if (index < 5) {
-                      return (
-                        <h1
-                          className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30"
-                          key={tag.id}
-                        >
-                          {tag.name}
-                        </h1>
-                      );
-                    }
-                  })}
-                </div>
-              </div>
-            )}
           </a>
         </Link>
+
+        {!loading && (
+          <div className="p-4">
+            <h1 className="text-2xl font-bold">{title}</h1>
+            <h3 className="mt-1 text-lg">{description}</h3>
+            <article className="mt-5 mb-3">
+              <h4 className="flex items-center text-xl font-bold gap-3">
+                <svg
+                  className="w-6 h-6"
+                  focusable="false"
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                >
+                  <path d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"></path>
+                </svg>
+                관련링크들
+              </h4>
+              <ul className="flex flex-col items-start gap-1 my-2">
+                <li className="flex items-center gap-1">
+                  <svg
+                    className="w-6 h-6 fill-blue-500"
+                    focusable="false"
+                    viewBox="0 0 24 24"
+                    aria-hidden="true"
+                  >
+                    <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"></path>
+                  </svg>
+                  <a href={github} target="_black" className="py-2">
+                    깃허브 바로가기
+                  </a>
+                </li>
+
+                <li className="flex items-center gap-1">
+                  <svg
+                    className="text-blue w-7 h-7 fill-blue-500"
+                    focusable="false"
+                    viewBox="0 0 24 24"
+                    aria-hidden="true"
+                  >
+                    <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"></path>
+                  </svg>
+                  <a href={notion} target="_black" className="py-2">
+                    노션 보러가기
+                  </a>
+                </li>
+              </ul>
+            </article>
+
+            <p className="my-1 py-2">
+              작업기간 : {start} ~ {end} ({calculatedPeriod(start, end)}일)
+            </p>
+            <div className="flex flex-wrap items-start gap-2 mt-2">
+              {tags?.map((tag: any, index: number) => {
+                if (index < 5) {
+                  return (
+                    <h1
+                      className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30"
+                      key={tag.id}
+                    >
+                      {tag.name}
+                    </h1>
+                  );
+                }
+              })}
+            </div>
+          </div>
+        )}
       </div>
     );
   } else {
